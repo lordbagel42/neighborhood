11cd9507bb29b8df9da96e3d4b511b53442fd871
lordbagel42 <raygenrrupe@gmail.com>
2025-05-08 17:11:32 -0600

    navbar

---
+++ src/lib/utils/constants.ts
@@ -0,0 +1,2 @@
+export const AppName: string = 'CollaborArt'; // not sure how much I like this name
+export const AppDescription: string = 'The collaborative art project';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
+
+<Navbar />
 
 {@render children()}
