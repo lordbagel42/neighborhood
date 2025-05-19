c1d2c3b9ecb57518750a652a5a897eff6883d025
lordbagel42 <raygenrrupe@gmail.com>
2025-05-18 19:08:00 -0600

    yeedaladeet

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,2 +1,2 @@
-export const AppName: string = 'CollaborArt'; // not sure how much I like this name
+export const AppName: string = 'art.bagelindustries.com'; // not sure how much I like this name
 export const AppDescription: string = 'The collaborative art project';
